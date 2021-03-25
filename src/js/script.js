589098d9ac9aed85a0e5b063187bd405e845e663
imran5644 <imi56444@gmail.com>
2021-03-26 00:33:16 +0800

    add the function constructor and add books to the console

--- src/js/script.js
+++ src/js/script.js
@@ -2,21 +2,29 @@ let myLibrary = [
     {
         title: 'Boss',
         author: 'Hritik Roshan',
-        pages: 210
+        noOfPages: 210
     },
     {
         title: 'Vampires Blood',
         author: 'Natasia Poleman',
-        pages: 456
+        noOfPage: 456
     }
 ];
 
-function Book() {
+function Book(title,author,noOfPage, read) {
   // the constructor...
+  this.title = title;
+  this.author = author;
+  this.noOfPage = noOfPage;
+  this.read = read;
 }
 
 function addBookToLibrary() {
   // do stuff here
+  const book = new Book('Vampires Blood', 'Natasia Poleman', 456)
+  myLibrary.push = book;
+
 }
 
 console.log(myLibrary);
+addBookToLibrary();
