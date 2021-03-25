db086a7adf007f32f60a5355a333e07325998c52
imran5644 <imi56444@gmail.com>
2021-03-26 00:23:29 +0800

    add the link for javascript in index.html

---
+++ src/js/script.js
@@ -0,0 +1,22 @@
+let myLibrary = [
+    {
+        title: 'Boss',
+        author: 'Hritik Roshan',
+        pages: 210
+    },
+    {
+        title: 'Vampires Blood',
+        author: 'Natasia Poleman',
+        pages: 456
+    }
+];
+
+function Book() {
+  // the constructor...
+}
+
+function addBookToLibrary() {
+  // do stuff here
+}
+
+console.log(myLibrary);
