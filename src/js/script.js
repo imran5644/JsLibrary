eaf92ba661c37a6be7084104c82ec0c3bda9169e
imran5644 <imi56444@gmail.com>
2021-03-26 01:17:37 +0800

    Add the listbook function to display books on browser

--- src/js/script.js
+++ src/js/script.js
@@ -2,12 +2,14 @@ let myLibrary = [
     {
         title: 'Boss',
         author: 'Hritik Roshan',
-        noOfPages: 210
+        noOfPage: 900,
+        read: false
     },
     {
         title: 'Vampires Blood',
         author: 'Natasia Poleman',
-        noOfPage: 456
+        noOfPage: 456,
+        read: true
     }
 ];
 
@@ -21,10 +23,41 @@ function Book(title,author,noOfPage, read) {
 
 function addBookToLibrary() {
   // do stuff here
-  const book = new Book('Vampires Blood', 'Natasia Poleman', 456)
+  const book = new Book('Vampires Blood', 'Natasia Poleman', 467)
   myLibrary.push = book;
 
 }
 
 console.log(myLibrary);
-addBookToLibrary();
+
+
+function listBooks() {
+    document.getElementById('list').innerHTML = '';
+    const d = document.getElementById('list');
+    for (let i = 0; i < myLibrary.length; i += 1) {
+      d.innerHTML += `
+      <div class="col-5">
+        <div class="card" style="width: 18rem;">
+          <div class="card-body">
+            <h5 class="card-title">${myLibrary[i].title} ( ${myLibrary[i].noOfPage} pages)</h5>
+            <h6 class="card-subtitle mb-2 text-muted">${myLibrary[i].author}</h6>
+            <p class="card-text">Have you read it before? ${myLibrary[i].read}</p>
+            <div class="d-grid gap-2">
+              <button class="btn btn-primary" onClick="deleteBook(${i})" class="card-link">Delete</button>
+              <button class="btn btn-primary" onClick="readBook(${i})" class="card-link">Change read</button>
+            </div>
+          </div>
+        </div>
+      </div>`;
+    }
+  }
+
+  function readBook(id) {
+    myLibrary[id].read = !myLibrary[id].read;
+    listBooks();
+  }
+
+  window.onload = function() {
+    listBooks()
+  };
+  
